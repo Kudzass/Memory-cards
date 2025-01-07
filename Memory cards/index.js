7f676a56a58e36b0b6b9d4fd1b1c56e8c63b394f
Name Surname“Tomas Sipaitisgit config --global user.name Tomas <t.sipaitis@gmail.com>
2025-01-07 22:21:25 +0200

    Function to shuffle the cards

--- Memory cards/index.js
+++ Memory cards/index.js
@@ -21,3 +21,10 @@ function startGame() {
     gameBoard.appendChild(card);
   }
 }
+function shuffle(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    let randomIndex = Math.floor(Math.random() * (i + 1));
+    [array[i], array[randomIndex]] = [array[randomIndex], array[i]];
+  }
+  return array;
+}
