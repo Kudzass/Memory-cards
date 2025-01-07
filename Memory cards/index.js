200dca023311700f714fcad01235e5d40106a348
Name Surname“Tomas Sipaitisgit config --global user.name Tomas <t.sipaitis@gmail.com>
2025-01-07 21:48:53 +0200

    Add game variables

--- Memory cards/index.js
+++ Memory cards/index.js
@@ -1,1 +1,6 @@
 const symbols = ["🌙", "🍀", "🍎", "🍓", "🍒", "🍇", "🍉", "⭐", "🔥"];
+let cards = [];
+let flippedCards = [];
+let matchedCards = [];
+let gameBoard = document.getElementById("game-board");
+let restartButton = document.getElementById("restart-button");
